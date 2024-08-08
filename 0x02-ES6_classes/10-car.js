8e38ef47c7feed0a7c4797bb0df4262298414bed
DanielJohn17 <danielyohannes38@gmail.com>
2024-08-08 21:18:44 +0300

    finished from task 6 upto task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+export default class Car {
+  constructor(brand, motor, color) {
+    this.brand = brand;
+    this.motor = motor;
+    this.color = color;
+  }
+
+  cloneCar() {
+    const Species = this.constructor[Symbol.species];
+
+    return new Species();
+  }
+}
