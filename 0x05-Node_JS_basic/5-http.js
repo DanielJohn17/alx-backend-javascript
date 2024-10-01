4bcababf09e792dc57c12de8f3828e05dd00c678
DanielJohn17 <danielyohannes38@gmail.com>
2024-10-01 23:33:18 +0300

    finished task 5

---
+++ 0x05-Node_JS_basic/5-http.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const hostname = '127.0.0.1';
+const port = 1245;
+
+const app = http.createServer((request, response) => {
+  response.statusCode = 200;
+  response.setHeader('Content-Type', 'text/plain');
+  if (request.url === '/') {
+    response.write('Hello Holberton School!');
+    response.end();
+  }
+  if (request.url === '/students') {
+    response.write('This is the list of our students\n');
+    countStudents(process.argv[2].toString()).then((output) => {
+      const outString = output.slice(0, -1);
+      response.end(outString);
+    }).catch(() => {
+      response.statusCode = 404;
+      response.end('Cannot load the database');
+    });
+  }
+});
+
+app.listen(port, hostname, () => {
+});
+
+module.exports = app;
